5e388b5077e7e1d3c5ba22b00f6e24b0b9dfe2d0
Jose <Jose@CACTUS>
2020-07-06 14:00:54 +0200

    LOG 4G Y EL AMBITO

--- src/CalcularIMC.java
+++ src/CalcularIMC.java
@@ -2,12 +2,17 @@
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
 
@@ -21,7 +26,22 @@ import edu.hedima.val.imc.TiposIMC;
 @WebServlet("/CalcularIMC")
 public class CalcularIMC extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private Logger log = Logger.getLogger("mylog");
+	private List<Persona> lp = new ArrayList<Persona>();
        
+	//SCOPE O ÁMBITO --> CUÁNTO TIEMPO VIVE UNA VARIABLE EN EL SERVIDOR
+	//REQUEST --> CADA VE QUE LLAMO SE CREA Y SE DESTRUYE
+	//SESIÓN  --> DURA LO QUE DURA LA SESIÓN
+	//CONTEXTO --> DURA LO QUE DURE LA APLICACIÓN
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		//lp = new ArrayList<Persona>();
+	}
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -45,6 +65,9 @@ public class CalcularIMC extends HttpServlet {
 		// TODO leer el peso y la altura recibidos y devolver el IMC
 		//al ser un POST , la info viene en el cuerpo. Hay que leer de ahí
 		
+		log.debug("Ha entrado en DoPost de CalcularIMC 2");
+		
+		
 		BufferedReader br = request.getReader();
 		String persona_json = br.readLine();
 		System.out.println("Persona json = " +persona_json);
@@ -56,6 +79,8 @@ public class CalcularIMC extends HttpServlet {
 		//vamos a pasar de string a persona
 		Gson gson = new Gson();
 		Persona p = gson.fromJson(persona_json, Persona.class);
+		this.lp.add(p);
+		
 		System.out.println("Peso persona = " +p.getPeso());
 		System.out.println("Altura persona = "+ p.getAltura());
 		double imc = IMC.calcula(p);
@@ -72,8 +97,23 @@ public class CalcularIMC extends HttpServlet {
 		String json_resultado = gson.toJson(imcResultado);
 		System.out.println("json devuelto =  "+ json_resultado);
 		
+		int npersonas = 1;
+		for (Persona persona : lp)
+		{
+			//log.debug("Nombre " + persona.getNombre());//todo registrar el nombre
+			log.debug("PERSONA " + npersonas);
+			log.debug("Peso " + persona.getPeso());
+			log.debug("Altura " + persona.getAltura());
+			npersonas++;
+		}
+		
+		
+		
+		response.setContentType("application/json; charset=UTF-8");//setea el tipo mime en la cabecera
+		response.setStatus (HttpURLConnection.HTTP_OK);//seteo el status
+		
 		//response.getWriter().append(strimc);
-		response.getWriter().append(json_resultado);
+		response.getWriter().append(json_resultado);//escribo el JSON en el cuerpo
 		
 		
 		
